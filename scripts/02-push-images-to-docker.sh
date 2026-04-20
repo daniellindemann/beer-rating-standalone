b87a7b07b5977a12b7a752cab9f3f288c22d4123
Daniel Lindemann <d.lindemann@abtis.de>
2026-04-20 10:35:53 +0000

    add docker push script

---
+++ scripts/02-push-images-to-docker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+script_dir=$(dirname "$0")
+
+# script variables
+backend_image='daniellindemann/beer-rating-backend'
+frontend_image='daniellindemann/beer-rating-frontend'
+console_image='daniellindemann/beer-rating-console-beerquotes'
+
+# push the images to docker hub
+images=(
+    "${backend_image}"
+    "${frontend_image}"
+    "${console_image}"
+)
+
+for image in "${images[@]}"; do
+    docker push -a "${image}:${tag}"
+done
+
+echo "All images pushed to Docker Hub."
